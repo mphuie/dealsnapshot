af9d610d61b598e189006d46ee0e921ad8440847
Matthew Huie <mphuie@gmail.com>
2012-11-16 11:13:15 -0800

    Added snapshots

--- deals.py
+++ deals.py
@@ -1,12 +1,15 @@
+#!/usr/bin/python
+
 import re
 import urllib2
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, Unicode
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, backref
 from datetime import datetime, timedelta
 
-engine = create_engine('sqlite:///sd.sqlite', echo=False)
+engine = create_engine('sqlite:////Users/mathuie/scripts/deals/sd.sqlite', echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -27,11 +30,22 @@ class Deal(Base):
 	def __repr__(self):
 		return "<Deal: %s / %s" % (self.title, str(self.create_date))
 
+class Snapshot(Base):
+	__tablename__ = 'dealsnapshot'
+	
+	id = Column(Integer, primary_key=True)
+	deal_id = Column(Integer, ForeignKey('deals.id'))
+	rating = Column(Integer)
+	views = Column(Integer)
+	replies = Column(Integer)
+	score = Column(Float)
+	deal = relationship("Deal", backref=backref('snapshots', order_by=id))
+
 Base.metadata.create_all(engine) 
 
 def parse_date(date):
-	date = re.sub("(?si)<.*?>", "", date)
-	date = re.sub("(?si)[\r\n]", "", date)
+	date = re.sub("(?si)<.*?>", "", date)	# strip html tags
+	date = re.sub("(?si)[\r\n]", "", date)	# strip newlines
 	date = re.sub(r"(?i)\s+", " ", date)
 	
 	if re.search("Today", date):
@@ -47,7 +61,7 @@ def parse_date(date):
 
 if __name__ == "__main__":
 	deals = []
-	response = urllib2.urlopen('http://slickdeals.net/forums/forumdisplay.php?f=9')
+	response = urllib2.urlopen('http://slickdeals.net/forums/forumdisplay.php?f=9&perpage=80')
 	html = response.read()
 	for match_deal in re.finditer(r'(?si)<tr id="sdpostrow_\d+">.*?</tr>', html):
 		deal_html = match_deal.group()
@@ -55,14 +69,38 @@ if __name__ == "__main__":
 		thread_id = match.group(1)
 		thread_title = match.group(2)
 		
-		thread_title = re.sub("[^ -~]", "", thread_title)
-	
-		match = re.search(r"<!-- Post Date -->\s*<td[^<]+>\s+<div[^<]+>(.*?)</span>", deal_html, re.DOTALL | re.IGNORECASE)
-		thread_date = parse_date(match.group(1))
+		replies = re.search(r"Replies:\s([\d,]+)", deal_html).group(1).replace(',','')
+		views = re.search(r"Views:\s([\d,]+)", deal_html).group(1).replace(',','')
+		match_votes = re.search(r"Votes:\s([\d,]+)", deal_html)
+		if match_votes:
+			votes = match_votes.group(1).replace(',','')
+		else:
+			votes = -1
+		match_score = re.search(r"Score:\s([\d,]+)", deal_html)
+		if match_score:
+			score = match_score.group(1).replace(',','')
+		else:
+			score = -1
 		
-		d = Deal(thread_id, thread_title, thread_date)
-		session.add(d)
-		print d.thread_id
+		
+		if not session.query(Deal).filter(Deal.thread_id == thread_id).count():
+			thread_title = re.sub("[^ -~]", "", thread_title)	# strip non-printable ascii characters
+			match = re.search(r"<!-- Post Date -->\s*<td[^<]+>\s+<div[^<]+>(.*?)</span>", deal_html, re.DOTALL | re.IGNORECASE)
+			thread_date = parse_date(match.group(1))
+		
+			d = Deal(thread_id, thread_title, thread_date)
+			session.add(d)
+			print "Added %s (replies: %s, views: %s, votes: %s, score %s)" % (d.thread_id, replies, views, votes, score)
+		else:
+			print "Already in database (%s)" % thread_id
+			
+		s = Snapshot()
+		s.replies = replies
+		s.views = views
+		s.match_votes = match_votes
+		s.score = score
+		s.deal = session.query(Deal).filter(Deal.thread_id == thread_id).first()
+		session.add(s)
 
 	session.commit()
 		
